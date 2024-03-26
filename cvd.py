cea54a8a844ed36c9423d966d8d639395f254eee
salmonGowa <gowasalmon@yahoo.com>
2024-03-26 03:47:40 -0400

    model

---
+++ cvd.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab as pl
+import scipy.optimize as opt
+import statsmodels.api as sm
+from sklearn import preprocessing
+import seaborn as sns
+import scipy.optimize as opt
+
+df=pd.read_csv('heart.csv')
+df.head()
+
+
